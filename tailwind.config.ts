d9dff1a0e9de70ddd6c1e39808a3802f9055fb7b
yutakanno-re-atika <yutakanno@re-atika.com>
2026-02-13 19:03:23 +0900

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        serif: ['"Noto Serif JP"', "serif"],
+        sans: ['"Inter"', "sans-serif"],
+        mono: ['"Oswald"', "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
